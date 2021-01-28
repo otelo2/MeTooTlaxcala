72ce012ce676052c2dd9953d71f73d0d7c583a0a
Otelo2 <josean2009@live.com.mx>
2021-01-28 13:07:05 -0600

    Correctly write tweets to file

--- tweetDownloader.py
+++ tweetDownloader.py
@@ -6,12 +6,17 @@ auth = tweepy.OAuthHandler(apiKey, apiSecret)
 auth.set_access_token(accessToken, accessTokenSecret)
 api = tweepy.API(auth)
 
-f = open("tweets.txt", "w")
+f = open("tweets.txt", "w", encoding="utf-8")
 
-for tweet in tweepy.Cursor(api.user_timeline, id='MeToo_Tlx', count='3000', exclude_replies=True, include_rts=False).items(3000):
-    print(tweet.text)
-    print('\n')
-    f.write(tweet.text)
-    f.write('\n')
+for tweet in tweepy.Cursor(api.user_timeline, id='MeToo_Tlx', exclude_replies=True, include_rts=False, tweet_mode='extended').items(3000):
+    try:
+        text = tweet.full_text
+        text = text.replace('\n',' -- ')
+        print(text)
+        print('\n')
+        f.write(text)
+        f.write('\n')
+    except UnicodeEncodeError:
+        print("potential emoji found")
 
 f.close()
