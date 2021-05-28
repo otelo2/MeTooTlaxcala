df01cb3ad669944e648bd147e5e061d1e7025b85
Otelo2 <josean2009@live.com.mx>
2021-05-28 15:51:41 -0500

    "Fix" bug from #1 (not really)

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from TweetDownloader import TweetDownloader
 def main():
     tweetDownloader = TweetDownloader()
     if (tweetDownloader.newTweet()):
-        #amount = tweetDownloader.numOfTweetsToDownload()
-        amount = 1000
+        amount = tweetDownloader.numOfTweetsToDownload()
         print(f'There are {amount} new tweets, starting download...')
         downloadImages = True
         tweetDownloader.processTweetsToFile(downloadImages, amount)
