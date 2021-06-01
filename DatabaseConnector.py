3e793961df8b929da45cf0666c40f99a5b17994f
Otelo2 <josean2009@live.com.mx>
2021-05-31 21:17:30 -0500

    Add the data from the tweets file to the database

--- DatabaseConnector.py
+++ DatabaseConnector.py
@@ -41,7 +41,7 @@ class DatabaseConnector:
         with conn:
             #Create the cursor. Result is in a dictionary
             with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
-                #id | nombre | apellidoPaterno | apellidoMaterno | denuncia | imagen1 | imagen2 | imagen3 | imagen4 | fecha | link | tweetID
+                #id | nombre | apellidoPaterno | apellidoMaterno | denuncia | imagen1 | imagen2 | imagen3 | imagen4 | fecha | hora | link | tweetID
                 #Just creates the table.
                 query = f"INSERT INTO abusador VALUES(DEFAULT, '{nombre}', '{apellidoPaterno}', '{apellidoMaterno}', '{denuncia}', \
                     '{imagen1}', '{imagen2}', '{imagen3}', '{imagen4}', '{fecha}', '{hora}', '{link}', '{tweetID}');"
@@ -53,7 +53,7 @@ class DatabaseConnector:
         with conn:
             #Create the cursor. Result is in a dictionary
             with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
-                #id | nombre | apellidoPaterno | apellidoMaterno | denuncia | imagen1 | imagen2 | imagen3 | imagen4 | fecha | link | tweetID
+                #id | nombre | apellidoPaterno | apellidoMaterno | denuncia | imagen1 | imagen2 | imagen3 | imagen4 | fecha | hora | link | tweetID
                 #Prints all the contents from the abusador table
                 nombre = "Santiago"
                 apellidoPaterno = "Yael"
@@ -77,7 +77,50 @@ class DatabaseConnector:
         with conn:
             #Create the cursor. Result is in a dictionary
             with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
-                #id | nombre | apellidoPaterno | apellidoMaterno | denuncia | imagen1 | imagen2 | imagen3 | imagen4 | fecha | link | tweetID
+                #id | nombre | apellidoPaterno | apellidoMaterno | denuncia | imagen1 | imagen2 | imagen3 | imagen4 | fecha | hora | link | tweetID
                 #Prints all the contents from the abusador table
                 cur.execute("SELECT * FROM abusador;")
                 print(cur.fetchall())
+
+    #Perform the query in the argument
+    def customQuery(query):
+        #Connect to the database
+        conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+        with conn:
+            #Create the cursor. Result is in a dictionary
+            with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+                #id | nombre | apellidoPaterno | apellidoMaterno | denuncia | imagen1 | imagen2 | imagen3 | imagen4 | fecha | hora | link | tweetID
+                #Performs a custom query
+                cur.execute(str(query))
+                print(cur.fetchall())
+
+    #Adds the contents from thw tweets.txt file to the databse
+    def tweetsFileToDatabase(self, verbose=True):
+        try:
+            with open("tweets.txt", "r", encoding="utf-8") as f:
+                lines = f.readlines()
+                for index, line in enumerate(lines):
+                    #Create a list of the relevant data
+                    line = line.split(",")
+                    #Put data into variables for verbosity
+                    nombre = line[0]
+                    apellidoPaterno = line[1]
+                    apellidoMaterno = line[2]
+                    denuncia = line[3]
+                    imagen1 = line[4]
+                    imagen2 = line[5]
+                    imagen3 = line[6]
+                    imagen4 = line[7]
+                    fecha = line[8]
+                    hora = line[9]
+                    link = line[10]
+                    tweetID = line[11]
+                    #Place the data in the database
+                    self.addToTable(nombre, apellidoPaterno, apellidoMaterno, denuncia, imagen1, imagen2, imagen3, imagen4, fecha, hora, link, tweetID)
+                    #Print the person who was just added
+                    if verbose:
+                        print(f"{index+1}: Added person {nombre} {apellidoPaterno} {apellidoMaterno}.")
+            if verbose:
+                print(f"Added {index+1} entries to the database")
+        except FileNotFoundError:
+            print("File not found error")
