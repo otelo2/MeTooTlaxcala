7bad67f63df47f97850b2c07ee42fbe996bf5c64
Otelo2 <josean2009@live.com.mx>
2021-06-02 23:21:18 -0500

    Remove NULL's from result

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -9,6 +9,28 @@ const searchAbusers = async searchText => {
     //Get matches to current text input
     let matches = abusers.filter(abuser => {
         const regex = new RegExp(`^${searchText}`, 'gi');
+        //Replace NULL's with nothing. This isn't good, but idc
+        if(abuser.nombre=="NULL"){
+            abuser.nombre=""
+        }
+        if(abuser.apellidopaterno=="NULL"){
+            abuser.apellidopaterno=""
+        }
+        if(abuser.apellidomaterno=="NULL"){
+            abuser.apellidomaterno=""
+        }
+        if(abuser.imagen1=="NULL"){
+            abuser.imagen1=""
+        }
+        if(abuser.imagen2=="NULL"){
+            abuser.imagen2=""
+        }
+        if(abuser.imagen3=="NULL"){
+            abuser.imagen3=""
+        }
+        if(abuser.imagen4=="NULL"){
+            abuser.imagen4=""
+        }
         return abuser.nombre.match(regex) || abuser.apellidopaterno.match(regex) || abuser.apellidomaterno.match(regex);
     });
 
